ec5cd384a0fc31315b2d7a966290246ebe9fa869
Satish Ch <satishchinthanippu@gmail.com>
2024-07-17 10:51:26 +0530

    Added teradata specific test setup

---
+++ dev/dags/teradata_cosmos_dag.py
@@ -0,0 +1,42 @@
+"""
+An example DAG that uses Cosmos to render a dbt project into an Airflow DAG using Teradata.
+"""
+
+import os
+from datetime import datetime
+from pathlib import Path
+
+from cosmos import DbtDag, ProfileConfig, ProjectConfig
+from cosmos.profiles import TeradataUserPasswordProfileMapping
+
+DEFAULT_DBT_ROOT_PATH = Path(__file__).parent / "dbt"
+DBT_ROOT_PATH = Path(os.getenv("DBT_ROOT_PATH", DEFAULT_DBT_ROOT_PATH))
+
+profile_config = ProfileConfig(
+    profile_name="default",
+    target_name="dev",
+    profile_mapping=TeradataUserPasswordProfileMapping(
+        conn_id="teradata_conn",
+        disable_event_tracking=True,
+    ),
+)
+
+# [START local_example]
+teradata_cosmos_dag = DbtDag(
+    # dbt/cosmos-specific parameters
+    project_config=ProjectConfig(
+        DBT_ROOT_PATH / "jaffle_shop",
+    ),
+    profile_config=profile_config,
+    operator_args={
+        "install_deps": True,  # install any necessary dependencies before running any dbt command
+        "full_refresh": True,  # used only in dbt commands that support this flag
+    },
+    # normal dag parameters
+    schedule_interval="@daily",
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    dag_id="basic_cosmos_dag",
+    default_args={"retries": 2},
+)
+# [END local_example]
